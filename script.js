42a4cfc062179f8930626289cd0196771b3a068b
Kunal Gupta <kunal22.10.2002@gmail.com>
2023-06-09 13:56:11 +0530

    close btn updated

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const popup = document.querySelector('.btn-login');
+const close = document.querySelector('.close-btn');
 
 registerLink.addEventListener('click', () => {
     wrapper.classList.add('active');
@@ -13,4 +14,8 @@ loginLink.addEventListener('click', () => {
 
 popup.addEventListener('click', () => {
     wrapper.classList.add('active-popup');
+})
+
+close.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');
 })
